af67ad79d73dfb26780f27607c7eb63a0df24262
Erlang <71025266+3RL4N9@users.noreply.github.com>
2020-09-11 23:05:25 +0700

    Add files via upload

---
+++ lagu.py
@@ -0,0 +1,127 @@
+#Hay Kak ini hanya tools abal2
+#Ini pake python
+#sudah itu aja
+
+import os
+import time
+
+
+def siapa():
+            os.system('clear')
+            nama = input('Hay Kak nama Kakak siapa?: ')
+            time.sleep(2.00)
+            print("Hay Kak",nama,)
+            time.sleep(2.00)
+            print("Bagaimana kabarnya Kak",nama,)
+            time.sleep(2.00)
+            print("Semoga Kak",nama,"Sehat Selalu")
+            time.sleep(2.00)
+siapa()
+def logo():
+           os.system('clear')
+           os.system('toilet -f standard Lagu -F gay')
+           time.sleep(2.00)
+           print("""
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█\33[35m
++++++++++++++++++++++++++++++++++++++++++++++++
++++++++++++++++++++++++++++++++++++++++++++++++
+|Author  :Erlang                              |
+|Facebook:Erlang Sans                         |
+|Github  :https://github.com/3RL4N9/          |
++++++++++++++++++++++++++++++++++++++++++++++++
++++++++++++++++++++++++++++++++++++++++++++++++\33[36m
+_______________________________________________
+|========|{•}Kumpulan link download lagu{•}   |
+|{?}MENU |{•}link akan menuju ke metrolagu{•} |
+|========|____________________________________|
+|{1}Hand Of Blood(Bulletin For My Valentine)  |
+|{2}So Far Away(Avanged Sevenfold)            |
+|{3}Nightmare(Avanged Savenfold)              |
+|{4}Dear God(Avanged Savenfold)               |
+|{5}Somthings Never Change(Queen)             |
+|{6}Wrap Me In plastic(Unkwon)                |
+|{7}Wind Of Change(Scorpion)                  |
+|{8}Selamat Jalan(Tipe-X)                     |
+|{9}Sakit Hati(Tipe-X)                        |
+|{10}Avanged Savenfold(Full Album)            |
+|{11}Tipe-X(Full Album)                       |
+|{12}Bulletin For My Valentine(Full Album)    |
+|{13}On My Way Versi Arab(unkwon)             |
+|{14}Selama Matahari Terbit Dari Timur(unkwon)|
+|{15}Kapan Ku Rabi(Wagu ndeso)                |
+|{0}Keluar                                    |
+|=============================================|
+""")
+logo()
+time.sleep(2.00)
+pilih = input('▶▶▶▶▶')
+
+if pilih == '1':
+         print("https://metrolagu.site/p.php?v=c0SrxSMHDmE&title=Bullet%20For%20My%20Valentine%20-%20Hand%20Of%20Blood")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '2':
+         print("https://metrolagu.site/p.php?v=A7ry4cx6HfY&title=Avenged%20Sevenfold%20-%20So%20Far%20Away")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '3':
+         print("https://metrolagu.site/p.php?v=94bGzWyHbu0&title=Avenged%20Sevenfold%20-%20Nightmare")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '4':
+         print("https://metrolagu.site/p.php?v=mzX0rhF8buo&title=Avenged%20Sevenfold%20-%20Dear%20God")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '5':
+         print("https://metrolagu.site/p.php?v=3o0mlzgR8Nc&title=Somethings%20Never%20Change")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '6':
+         print("https://metrolagu.site/p.php?v=QUO80N-rj0k&title=Wrap%20Me%20In%20Plastic%20-%20Chromance%20%20Lyrics%20Video%20Dan%20Terjemahan")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '7':
+         print("https://metrolagu.site/p.php?v=n4RjJKxsamQ&title=Scorpions%20-%20Wind%20Of%20Change")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '8':
+         print("https://metrolagu.site/p.php?v=YwvJJBEHUGc&title=Tipe-x%20-%20Selamat%20Jalan%20|")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '9':
+         print("https://metrolagu.site/p.php?v=sB67NhUWhco&title=Tipe-x%20-%20Sakit%20Hati%20|")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '10':
+         print("https://metrolagu.site/p.php?v=jGO0ZI3zqcw&title=Avenged%20Sevenfold%20Greatest%20Hits%20%20Full%20Album%20%20-%20The%20Best%20Of%20Avenged%20Sevenfold%20%20Playlist")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '11':
+         print("https://metrolagu.site/p.php?v=O68WCgRnHOg&title=Best%2035%20Lagu%20Terpopuler%20Tipe%20X%20Full%20Album%20-%20%20Automusic%20Id")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '12':
+         print("https://metrolagu.site/p.php?v=XKgcoAGEExM&title=Bullet%20For%20My%20Valentine%20Greatest%20Hits%20|%20Best%20Of%20Bullet%20For%20My%20Valentine%20Full%20Album")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '13':
+         print("https://metrolagu.site/p.php?v=JnzUMd8Qx0k&title=On%20My%20Way%20Arab%20Gokil%20Parah%20%20Pubg%20Ngawur%20Mantav!%20|%203way%20Asiska%20%20Cover")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '14':
+         print("https://metrolagu.site/p.php?v=uVHKpJkwEJM&title=Selama%20Matahari%20Terbit%20Dari%20Timur,%20Cinta%20Ku%20Padamu%20Takan%20Pernah%20Luntur")
+         print(".")
+         print("")
+if pilih == '15':
+         print("https://metrolagu.site/p.php?v=mJS-hBhmVEg&title=Kapan%20Ku%20Rabi%20-%20Wagu%20Lagu%20%20%20Musik%20Video%20%20-%20Lagu%20Lucu")
+         print(".")
+         print("Salin Link Trus Tempel Di Browser Atau Google Atau Di Chrome")
+if pilih == '0':
+         os.system('clear')
+         print("Terima Kasi Kak Sudah Mau Makai Tools ini")
+         print(".")
+         print("Sampai Jumpa")
